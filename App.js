fe7e08c2ba4b4a2f6c8a41db2bdc068b35c0845e
Zi Huijie <zi.huijie@gmail.com>
2017-10-16 14:55:35 -0600

    branch-4

--- App.js
+++ App.js
@@ -3,72 +3,36 @@ import {
   StyleSheet,
   Text,
   View,
-  Animated,
-  Dimensions
+  TextInput
 } from 'react-native';
 
 import Video from 'react-native-video';
-import Sample from './sample.mp4';
-
-import Icon from 'react-native-vector-icons/FontAwesome';
+import LightVideo from './lights.mp4';
 
 export default class App extends Component<{}> {
 
-  state = {
-    buffering: true,
-    animated: new Animated.Value(0)
-  };
-handleLoadStart = () => {
-  this.triggerBufferAnimation();
-}
-
-triggerBufferAnimation = () => {
-  this.loopingAnimation = Animated.loop(
-    Animated.timing(this.state.animated, {
-      toValue: 1,
-      duration: 350,
-    })
-  ).start();
-}
-
-handleBuffer = (meta) => {
-  meta.isBuffering && this.triggerBufferAnimation();
-
-  if (this.loopingAnimation && !meta.isBuffering) {
-    this.loopingAnimation.stopAnimation();
-  }
-  this.setState({
-    buffering: meta.isBuffering
-  })
-}
   render() {
-    const { width } = Dimensions.get("window");
-    const height = width * 0.5625;
-  const { buffering } = this.state;
-  const interpolatedAnimation = this.state.animated.interpolate({
-    inputRange: [0,1],
-    outputRange: ["0deg", "360deg"]
-  });
 
-  const rotateStyle = {
-    transform: [
-      { rotate: interpolatedAnimation },
-    ]
-  }
     return (
       <View style={styles.container}>
-      <View style={buffering ? styles.buffering : undefined} >
-      <Video source={Sample}
-      resizeMode="contain"
-      onLoadStart={this.handleLoadStart}
-      onBuffer={this.handleBuffer}
-      />
-      <View style={styles.videoCover}>
-     {buffering && <Animated.View style={rotateStyle}>
-      <Icon name="circle-o-notch" size={30} color="#fff" />
-      </Animated.View>}
-      </View>
 
+      <Video
+      repeat
+      source={LightVideo}
+      resizeMode="cover"
+      style={StyleSheet.absoluteFill}
+      />
+      <View>
+      <Text style={styles.header}>Login</Text>
+      <TextInput
+      placeholder="Email"
+      style={styles.input}
+      />
+      <TextInput
+      placeholder="Password"
+      secureTextEntry
+      style={styles.input}
+      />
       </View>
       </View>
     );
@@ -78,7 +42,8 @@ handleBuffer = (meta) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 250,
+    alignItems: 'center',
+    justifyContent:'center',
   },
   videoCover: {
     alignItems: "center",
@@ -90,7 +55,16 @@ const styles = StyleSheet.create({
     bottom: 0,
     backgroundColor: "transparent",
   },
-  buffering: {
-    backgroundColor:"#000",
+  header: {
+    fontSize: 30,
+    backgroundColor:"transparent",
+    color: '#fff',
   },
+  input: {
+    width: 300,
+    height: 50,
+    backgroundColor:'#fff',
+    marginVertical: 15,
+    paddingLeft: 15,
+  }
 });
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ how to use react-native-video
 branch-1 setup & background video
 branch-2 video error
 branch-3 Buffer animated
+branch-4 Looping background video
+        Textinput
